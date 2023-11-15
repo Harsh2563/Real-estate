fb1e4682e42eaccf0214f2aa7892f8ca45de8db6
Harsh Rai <htc38199@gmail.com>
2023-11-15 14:53:28 +0530

    sign out

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -108,7 +108,7 @@ export default function Profile() {
   const handleSignOut = async () => {
     try {
       dispatch(signOutUserStart());
-      const res = await fetch('/api/auth/signout');
+      const res = await fetch('/api/user/signout');
       const data = await res.json();
       if (data.success === false) {
         dispatch(deleteUserFailure(data.message));
